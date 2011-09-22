f4bafb2381658e75c968b057758dfca458557e3a
Matthias Hryniszak <padcom@gmail.com>
2011-09-22 23:46:30 +0200

    Added missing parameter to getPerson call

--- frontend/grails-app/controllers/HomeController.groovy
+++ frontend/grails-app/controllers/HomeController.groovy
@@ -3,7 +3,7 @@ import com.aplaline.example.person.client.impl.JerseyPersonClient
 
 class HomeController {
 	def index = {
-		def person = new JerseyPersonClient("http://localhost:8080/services-person/api").getPerson()
+		def person = new JerseyPersonClient("http://localhost:8080/services-person/api").getPerson(123L)
 		[ message: "Hello, world!" ]
 	}
 }
